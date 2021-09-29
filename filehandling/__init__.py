8063ed568f52a4848d0729c67db6e8759ef6adcb
Red Lhota <11944660+roseasromeo@users.noreply.github.com>
2021-09-29 15:41:07 -0700

    implemented filename parsing and tests that it produces results

--- filehandling/__init__.py
+++ filehandling/__init__.py
@@ -1,9 +1,39 @@
-def filename_parse(filename, fname_format, sampleinfo_format, fname_split, sample_split):
+def foldername_parse(foldername, fname_format, sampleinfo_format, fname_split="_", sample_split='-'):
 
-    file_dict = {
-        "date": 20210929,
-        "fps": 25000,
-        "run": 1
-    }
+    # file_dict = {
+    #     "date" : "20210929",
+    #     "mw" : "6M",
+    #     "backbone" : "PEO",
+    #     "fps" : 25000,
+    #     "run" : 1
+    # }
+    folder_split = foldername.split(fname_split)
+    tag_split = fname_format.split(fname_split)
+
+    file_dict = {}
+
+    i = 0 # index in the folder name
+    for tag in tag_split:
+        value = folder_split[i] # entry in the folder name corresponding to the tag from the fname_format
+
+        if tag.lower() == "fps":
+            if "k" in value:
+                fps = int(''.join(i for i in value if i.isdigit()))* 1000 # take numeric part of fps and multiply by 1000 if k was used, i.e. 25k becomes 25000
+            else:
+                fps = int(''.join(i for i in value if i.isdigit())) # take numeric part of fps only
+            file_dict["fps"] = fps
+        elif tag.lower() == "run":
+            file_dict["run"] = int(''.join(i for i in value if i.isdigit())) # take numeric part of run only
+        elif tag == "sampleinfo":
+            sampleinfo_split = value.split(sample_split)
+            sample_tag_split = sampleinfo_format.split(sample_split)
+            j = 0
+            for sample_tag in sample_tag_split:
+                sample_value = sampleinfo_split[j] # entry within sampleinfo coresponding to the sample_tag from the sampleinfo_format
+                file_dict[sample_tag] = sample_value
+                j = j + 1
+        else:
+            file_dict[tag] = value
+        i = i + 1
 
     return file_dict
--- tests/unit/test_file_handling.py
+++ tests/unit/test_file_handling.py
@@ -1,11 +1,29 @@
 import filehandling as fh
 
-class TestFilenameParse:
-    filename  = "20210929_6M-PEO_fps-25k_1"
+class TestFoldernameParse:
+    # sample data to test against
+    folder  = "20210929_6M-PEO_fps-25k_1"
+    folder_nok  = "20210929_6M-PEO_fps25000_1"
     fname_format = "date_sampleinfo_fps_run"
     sampleinfo_format = "mw-backbone"
     fname_split = "_"
     sample_split = "-"
+    file_dict = {
+        "date" : "20210929",
+        "mw" : "6M",
+        "backbone" : "PEO",
+        "fps" : 25000,
+        "run" : 1
+    }
 
     def test_returns_dict(self):
-        assert type(fh.filename_parse(self.filename,self.fname_format,self.sampleinfo_format,self.fname_split,self.sample_split)) is dict
+        # fails if the foldername_parse method does not return a dictionary
+        assert type(fh.foldername_parse(self.folder,self.fname_format,self.sampleinfo_format,self.fname_split,self.sample_split)) is dict
+
+    def test_correct_parse_fps_k(self):
+        # fails if the foldername_parse method does not return the correct entry (case for testing when fps is formatted with a k)
+        assert fh.foldername_parse(self.folder,self.fname_format,self.sampleinfo_format,self.fname_split,self.sample_split) == self.file_dict
+
+    def test_correct_parse_fps_nok(self):
+        # fails if the foldername_parse method does not return the correct entry (case for testing when fps is formatted without a k)
+        assert fh.foldername_parse(self.folder_nok,self.fname_format,self.sampleinfo_format,self.fname_split,self.sample_split) == self.file_dict

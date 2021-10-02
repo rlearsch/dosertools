e8af6494b72c7d36cd038482e3b08af6d72fc2bc
Red Lhota <136386419+rlhota@users.noreply.github.com>
2021-10-02 10:38:51 -0700

    added panda and numpy, find_closest_value

---
+++ dataprocessing/array.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+def closest_index_for_value(dataset : pd.Dataframe,column : str,value : float) -> int:
+    closest_index = np.abs(dataset[column]-value).idxmin(axis=0)
+    return closest_index

a496cd012de6165b94fdcc9214b2ebab578f92f1
Red Lhota <136386419+rlhota@users.noreply.github.com>
2021-10-02 15:12:12 -0700

    test for correct value returned from closest_index_for_value

--- tests/unit/test_data_processing.py
+++ tests/unit/test_data_processing.py
@@ -3,20 +3,28 @@ import pandas as pd
 
 class TestClosestIndexFromValue:
     """
-    Tests for closest_index_from_value
+    Tests for closest_index_for_value
 
 
     Tests
     -----
+    test_returns_int:
+    checks that closest_index_for_value returns an integer
 
+    test_correct_index:
+    checks that closest_index_for_value returns the correct index
 
 
     """
 
     # sample data to test against
     column = 'value'
-    data = pd.Dataframe({self.column:[0,1,2]})
+    data = pd.DataFrame({column:[-1,0,1,2]})
 
     def test_returns_int(self):
-        # fails if the closest_index_from_value method does not return a integer
+        # fails if the closest_index_for_value method does not return a integer
         assert type(dp.array.closest_index_for_value(self.data,self.column,1.1)) is int
+
+    def test_correct_index(self):
+        # fails if the closest_index_for_value does not return 2 (corresponding to 1)
+        assert dp.array.closest_index_for_value(self.data,self.column,1.1) == 2

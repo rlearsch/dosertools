6fd5657da04e005fcfdc888c558ee08ba794a5dd
Red Lhota <136386419+rlhota@users.noreply.github.com>
2021-10-04 14:58:08 -0700

    implemented is_array_numeric, nonzero and zero runs with testing

--- tests/unit/test_data_processing.py
+++ tests/unit/test_data_processing.py
@@ -37,7 +37,7 @@ class TestClosestIndexForValue:
     def test_column_not_numeric(self):
         datatext = pd.DataFrame({self.column:["one"]})
         with pytest.raises(TypeError):
-            dp.array.closest_index_for_value(datatext,self.column,1.1) == 1
+            dp.array.closest_index_for_value(datatext,self.column,1.1)
         # handle if column does not contain numeric values
 
 class TestNonzeroRuns:
@@ -46,8 +46,16 @@ class TestNonzeroRuns:
 
     Tests
     -----
+    test_returns_array:
+        checks that nonzero_runs returns an array
 
+    test_correct_nonzero_runs:
+        checks that nonzero_runs returns the correct shape and elements given
+        a test array
 
+    test_string_array:
+        checks that nonzero_runs raises a TypeError if a non-numeric array
+        is used
     """
 
     #sample data to test against
@@ -60,13 +68,52 @@ class TestNonzeroRuns:
     def test_correct_nonzero_runs(self):
         # fails if the nonzero_runs method does not produce the correct indices
         # for the given array's nonzero runs
-        assert np.array_equal(dp.array.nonzero_runs(self.array), [[1,5],[10,12],[15,15]])
+        assert np.array_equal(dp.array.nonzero_runs(self.array), [[1,6],[10,13],[15,16]])
 
     def test_string_array(self):
         # fails if the nonzero_runs method does not produce the correct indices
         # for the given array's nonzero runs
         arraytext = ["one","two"]
-        assert dp.array.nonzero_runs(arraytext)
+        with pytest.raises(TypeError):
+            dp.array.nonzero_runs(arraytext)
+
+class TestZeroRuns:
+    """
+    Test zero_runs
+
+    Tests
+    -----
+    test_returns_array:
+        checks that zero_runs returns an array
+
+    test_correct_zero_runs:
+        checks that zero_runs returns the correct shape and elements given
+        a test array
+
+    test_string_array:
+        checks that zero_runs raises a TypeError if a non-numeric array
+        is used
+    """
+
+    #sample data to test against
+    array = [0, .1, .2, .1, 1, 2, 0, 0, 0, 0, 1, 2, 1, 0, 0, 1]
+
+    def test_returns_array(self):
+        # fails if the nonzero_runs method does not return an array
+        assert type(dp.array.zero_runs(self.array)) is np.ndarray
+
+    def test_correct_zero_runs(self):
+        # fails if the nonzero_runs method does not produce the correct indices
+        # for the given array's nonzero runs
+        assert np.array_equal(dp.array.zero_runs(self.array), [[0,1],[6,10],[13,15]])
+
+    def test_string_array(self):
+        # fails if the nonzero_runs method does not produce the correct indices
+        # for the given array's nonzero runs
+        arraytext = ["one","two"]
+        with pytest.raises(TypeError):
+            dp.array.zero_runs(arraytext)
+
 
 class TestIsDataFrameColumnNumeric:
     """
@@ -74,8 +121,11 @@ class TestIsDataFrameColumnNumeric:
 
     Tests
     -----
-    test_column_not_numeric
-        test if raises TypeError if dataframe column is not numeric
+    test_numeric_column
+        check if True if dataframe column is numeric
+
+    test_nonnumeric_column
+        check if False if dataframe column is not numeric
 
     """
 
@@ -83,28 +133,36 @@ class TestIsDataFrameColumnNumeric:
     column = 'value'
     data = pd.DataFrame({column:[-1,0,1,2]})
 
-    def test_column_numeric(self):
+    def test_numeric_column(self):
         # fails if function raises error on numeric column
         assert dp.array.is_dataframe_column_numeric(self.data,self.column)
 
-    def test_column_not_numeric(self):
+    def test_nonnumeric_column(self):
         # if non-numeric DataFrame column passed to function, raise TypeError
         datatext = pd.DataFrame({self.column:["one"]})
-        with pytest.raises(TypeError):
-            dp.array.is_dataframe_column_numeric(datatext,self.column)
+        assert not dp.array.is_dataframe_column_numeric(datatext,self.column)
         # handle if column does not contain numeric values
 
 class TestIsArrayNumeric:
-    """
+        """
+        Test is_array_numeric
 
-    """
+        Tests
+        -----
+        test_numeric_array
+            check if True if array is numeric
 
-    def test_numeric_array_pass:
+        test_nonnumeric_array
+            check if False if array is not numeric
 
+        """
+
+    def test_numeric_array(self):
+        arrays = [[1,2,3],[1.1,-1.2]]
         for array in arrays:
             assert dp.array.is_array_numeric(array)
 
-    def test_nonnumeric_array_error:
+    def test_nonnumeric_array(self):
+        arrays = [[object()],['string'],[None],[u'unicode']]
         for array in arrays:
-            with pytest.raises(TypeError):
-                dp.array.is_array_numeric(array)
+            assert not dp.array.is_array_numeric(array)

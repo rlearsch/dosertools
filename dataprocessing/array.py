139862045901c6d44c4b3492dd8a3c468c642803
Red Lhota <136386419+rlhota@users.noreply.github.com>
2021-10-04 13:22:04 -0700

    in progress: nonzero and zero runs

--- dataprocessing/array.py
+++ dataprocessing/array.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def closest_index_for_value(dataset : pd.DataFrame, column : str, value : float) -> int:
     """
+    Find the closest value for a given value in a column and returns its index
 
     Parameters
     ----------
@@ -16,22 +17,98 @@ def closest_index_for_value(dataset : pd.DataFrame, column : str, value : float)
         numeric value to compare entries in dataset[column] to
 
     """
-    # check if column is numeric
-    num_df = pd.DataFrame(columns=['int','float'])
-    num_df['int'] = num_df['int'].astype('int')
-    num_df['float'] = num_df['float'].astype('float')
-    if dataset[column].dtypes != num_df['int'].dtypes and dataset[column].dtypes != num_df['float'].dtypes:
-        raise TypeError("Column must be numeric")
+
+    is_dataframe_column_numeric(dataset, column)
+
+    # return closest_index by looking for smallest absolute distance from zero
+    # after subtraction
     closest_index = np.abs(dataset[column]-value).idxmin(axis=0)
     return closest_index
 
-def nonzero_runs(array : np.array) -> np.array:
+def nonzero_runs(array : np.ndarray) -> np.ndarray:
+    """
+    Return array with index pairs indicating runs of nonzero in given array
+
+    Returns array with shape (m, 2), where m is the number of "runs"
+    of non-zeros.
+    The first column is the index of the first non-zero,
+    the second is the index of the final non-zero.
+
+
+    Parameters
+    ----------
+    array : np.array
+        array to look for nonzero runs within
+
+    """
+
     ## Returns array with shape (m, 2), where m is the number of "runs" of non-zeros
     ## the first column sis the index of the first non-zero, the second is the index
     ## of the final non-zero
     ## modified from stackexchange comment
+
+
+
     contains_one = np.concatenate(([0], (~np.equal(array, 0).view(np.int8))+2, [0]))
     absdiff = np.abs(np.diff(contains_one))
     # Runs start and end where absdiff is 1.
     ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
     return ranges
+
+def zero_runs(array : np.ndarray) -> np.ndarray:
+    """
+    Return array with index pairs indicating runs of zero in given array
+
+    Returns array with shape (m, 2), where m is the number of "runs"
+    of zeros.
+    The first column is the index of the first zero,
+    ??the second is the index of the final zero.??
+
+
+    Parameters
+    ----------
+    array : np.array
+        array to look for zero runs within
+
+    """
+
+    # # it creates an array with shape (m, 2), where m is the number of "runs" of zeros.
+    ## The first column is the index of the first 0 in each run, and the second is the index of the first nonzero element after the run
+
+    # Create an array that is 1 where a is 0, and pad each end with an extra 0.
+    iszero = np.concatenate(([0], np.equal(array, 0).view(np.int8), [0]))
+    absdiff = np.abs(np.diff(iszero))
+    # Runs start and end where absdiff is 1.
+    ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
+
+def is_array_numeric(array : np.ndarray):
+    """
+
+    """
+
+    pass
+
+
+def is_dataframe_column_numeric(dataset : pd.DataFrame, column : str):
+    """
+    Raise exception if column in dataset is not numeric
+
+    Parameters
+    ----------
+
+    dataset : pandas.DataFrame
+        the dataframe that contains at least the column "column"
+        column must be numeric
+    column : str
+        name of column to look the closest value in
+    """
+
+
+    # check if column is numeric
+    num_df = pd.DataFrame(columns=['int','float'])
+    num_df['int'] = num_df['int'].astype('int')
+    num_df['float'] = num_df['float'].astype('float')
+    if dataset[column].dtypes != num_df['int'].dtypes and dataset[column].dtypes != num_df['float'].dtypes:
+        raise TypeError("dataset[column] must be numeric")
+
+    return True
--- tests/unit/test_data_processing.py
+++ tests/unit/test_data_processing.py
@@ -1,20 +1,24 @@
 import dataprocessing as dp
 import pandas as pd
 import pytest
+import numpy as np
 
 class TestClosestIndexForValue:
     """
-    Tests for closest_index_for_value
+    Test closest_index_for_value
 
 
     Tests
     -----
     test_returns_int:
-    checks that closest_index_for_value returns an integer
+        checks that closest_index_for_value returns an integer
 
     test_correct_index:
-    checks that closest_index_for_value returns the correct index
+        checks that closest_index_for_value returns the correct index
 
+    test_column_not_float:
+        checks that closest_index_for_value raises a TypeError if the specified
+        column does not contain numbers (for pandas.DataFrame, int64 or float)
 
     """
 
@@ -30,8 +34,77 @@ class TestClosestIndexForValue:
         # fails if the closest_index_for_value does not return 2 (corresponding to 1)
         assert dp.array.closest_index_for_value(self.data,self.column,1.1) == 2
 
-    def test_column_not_float(self):
+    def test_column_not_numeric(self):
         datatext = pd.DataFrame({self.column:["one"]})
         with pytest.raises(TypeError):
             dp.array.closest_index_for_value(datatext,self.column,1.1) == 1
         # handle if column does not contain numeric values
+
+class TestNonzeroRuns:
+    """
+    Test nonzero_runs
+
+    Tests
+    -----
+
+
+    """
+
+    #sample data to test against
+    array = [0, .1, .2, .1, 1, 2, 0, 0, 0, 0, 1, 2, 1, 0, 0, 1]
+
+    def test_returns_array(self):
+        # fails if the nonzero_runs method does not return an array
+        assert type(dp.array.nonzero_runs(self.array)) is np.ndarray
+
+    def test_correct_nonzero_runs(self):
+        # fails if the nonzero_runs method does not produce the correct indices
+        # for the given array's nonzero runs
+        assert np.array_equal(dp.array.nonzero_runs(self.array), [[1,5],[10,12],[15,15]])
+
+    def test_string_array(self):
+        # fails if the nonzero_runs method does not produce the correct indices
+        # for the given array's nonzero runs
+        arraytext = ["one","two"]
+        assert dp.array.nonzero_runs(arraytext)
+
+class TestIsDataFrameColumnNumeric:
+    """
+    Test is_dataframe_column_numeric
+
+    Tests
+    -----
+    test_column_not_numeric
+        test if raises TypeError if dataframe column is not numeric
+
+    """
+
+    # sample data to test against
+    column = 'value'
+    data = pd.DataFrame({column:[-1,0,1,2]})
+
+    def test_column_numeric(self):
+        # fails if function raises error on numeric column
+        assert dp.array.is_dataframe_column_numeric(self.data,self.column)
+
+    def test_column_not_numeric(self):
+        # if non-numeric DataFrame column passed to function, raise TypeError
+        datatext = pd.DataFrame({self.column:["one"]})
+        with pytest.raises(TypeError):
+            dp.array.is_dataframe_column_numeric(datatext,self.column)
+        # handle if column does not contain numeric values
+
+class TestIsArrayNumeric:
+    """
+
+    """
+
+    def test_numeric_array_pass:
+
+        for array in arrays:
+            assert dp.array.is_array_numeric(array)
+
+    def test_nonnumeric_array_error:
+        for array in arrays:
+            with pytest.raises(TypeError):
+                dp.array.is_array_numeric(array)

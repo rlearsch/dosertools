158efc9a29c0844129755c974bc29c9bb8e2837b
Robert Learsch <rlearsch@caltech.edu>
2021-10-05 13:09:51 -0700

    Improved testing, added target bg_median file

--- image_processing/test_tiff_handling.py
+++ image_processing/test_tiff_handling.py
@@ -1,5 +1,8 @@
 import image_processing.tiff_handling as th
 import skimage.io
+import skimage.filters
+from skimage.filters import (threshold_otsu, threshold_li)
+import numpy as np
 
 def test_define_initial_parameters():
     "Initial coefficients for first crop iterations"
@@ -21,10 +24,21 @@ def test_tiff_folder_path_correction():
 #    assert th.tiff_folder_to_image_collection("example_experimental_video") == example_experimenal_video
 #
 def test_define_image_parameters():
-    pass
+    background_video = skimage.io.imread_collection("image_processing//example_background_video//*", plugin='tifffile')
+    params_dict = th.define_initial_parameters()
+    params_dict = th.define_image_parameters(background_video, params_dict)
+    assert params_dict["crop_width_start"] == 86
+    assert params_dict["crop_width_end"] == 415
+    assert params_dict["crop_bottom"] == 634          
+    assert params_dict["crop_top"] == 47              
 
 def test_produce_background_image():
-    pass
+    background_video = skimage.io.imread_collection("image_processing//example_background_video//*", plugin='tifffile')
+    params_dict = th.define_initial_parameters()
+    params_dict = th.define_image_parameters(background_video, params_dict)
+    bg_median_test = th.produce_background_image(background_video, params_dict)
+    target_bg_median_array = np.load("image_processing//bg_median_array.npy")
+    assert np.all(target_bg_median_array == bg_median_test)
 
 def test_convert_tiff_sequence_to_binary():
     pass

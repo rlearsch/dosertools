1ca8cd927ee7b71d42626858e1af7c112b2b5447
Robert Learsch <rlearsch@caltech.edu>
2021-10-07 11:40:13 -0700

    Rewritten save_image() with more simple implementation

--- image_processing/test_tiff_handling.py
+++ image_processing/test_tiff_handling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import json
 
 import skimage.io
 import skimage.filters
@@ -19,52 +20,56 @@ def test_define_initial_parameters():
      
 
 def test_define_image_parameters():
-    background_video = skimage.io.imread_collection("image_processing//example_background_video//*", plugin='tifffile')
+    background_video_location = os.path.join("image_processing", "example_background_video", "*")
+    background_video = skimage.io.imread_collection(background_video_location, plugin='tifffile')
     params_dict = th.define_initial_parameters()
     params_dict = th.define_image_parameters(background_video, params_dict)
-    assert type(params_dict["crop_width_start"]) is int
-    assert type(params_dict["crop_width_end"]) is int
-    assert type(params_dict["crop_bottom"]) is int
-    assert type(params_dict["crop_top"]) is int
-                       
-    assert params_dict["crop_width_start"] == 86
-    assert params_dict["crop_width_end"] == 415
-    assert params_dict["crop_bottom"] == 634          
-    assert params_dict["crop_top"] == 47
+    
+    with open(os.path.join("image_processing","params.json")) as f:
+        target_params_dict = json.load(f)
+    assert target_params_dict == params_dict
+    #assert type(params_dict["crop_width_start"]) is int
+    #assert type(params_dict["crop_width_end"]) is int
+    #assert type(params_dict["crop_bottom"]) is int
+    #assert type(params_dict["crop_top"]) is int
+    #                   
+    #assert params_dict["crop_width_start"] == 86
+    #assert params_dict["crop_width_end"] == 415
+    #assert params_dict["crop_bottom"] == 634          
+    #assert params_dict["crop_top"] == 47
     
 def test_convert_tiff_image_saves_intermediate_files():
-    ### should I rewrite these file locations in a more neutral way, eg, os.path.join(...) ? ###
-    
-    if os.path.exists("image_processing//test_processed_images//crop//420.tiff"):
-        os.remove("image_processing//test_processed_images//crop//420.tiff")
-    if os.path.exists("image_processing//test_processed_images//bg_sub//420.tiff"):
-        os.remove("image_processing//test_processed_images//bg_sub//420.tiff")
-    if os.path.exists("image_processing//test_processed_images//bin//420.png"):
-        os.remove("image_processing//test_processed_images//bin//420.png")
+   
+    if os.path.exists(os.path.join("image_processing","test_processed_images","crop","420.tiff")):
+        os.remove(os.path.join("image_processing","test_processed_images","crop","420.tiff"))
+    if os.path.exists(os.path.join("image_processing","test_processed_images","bg_sub","420.tiff")):
+        os.remove(os.path.join("image_processing","test_processed_images","bg_sub","420.tiff"))
+    if os.path.exists(os.path.join("image_processing","test_processed_images","bin","420.tiff")):
+        os.remove(os.path.join("image_processing","test_processed_images","bin","420.tiff"))
      
-    background_video = skimage.io.imread_collection("image_processing//example_background_video//*", plugin='tifffile')
-    params_dict = th.define_initial_parameters()
-    params_dict = th.define_image_parameters(background_video, params_dict)
-    bg_median = th.produce_background_image(background_video, params_dict)
-    
-    image = skimage.io.imread("image_processing//example_experimental_video//2021-09-16_RWL-I-158-1_2.6MDa-TPAM-1.0wtpct-0.0x-NiCl_22G_shutter-50k_fps-25k_DOS-Al_4_exp_2109_1723000100.tif")
-    image_number = 100
-    save_location = "image_processing//test_processed_images"
+    bg_median = np.load("image_processing//bg_median_array.npy")
+    with open(os.path.join("image_processing","params.json")) as f:
+        params_dict = json.load(f)
+        
+    image_location = os.path.join("image_processing","example_experimental_video","2021-09-16_RWL-I-158-1_2.6MDa-TPAM-1.0wtpct-0.0x-NiCl_22G_shutter-50k_fps-25k_DOS-Al_4_exp_2109_1723000420.tif")
+    image = skimage.io.imread(image_location)
+    image_number = 420
+    save_location = os.path.join("image_processing","test_processed_images")
     save_crop = True
     save_bg_subtract = True
     th.convert_tiff_image(image, bg_median, params_dict, image_number, save_location, save_crop,save_bg_subtract)
-    assert os.path.exists("image_processing//test_processed_images//crop//100.tiff")
-    assert os.path.exists("image_processing//test_processed_images//bg_sub//100.tiff")
-    assert os.path.exists("image_processing//test_processed_images//bin//100.png")
+    assert os.path.exists(os.path.join("image_processing","test_processed_images","crop","420.tiff"))
+    assert os.path.exists(os.path.join("image_processing","test_processed_images","bg_sub","420.tiff"))
+    assert os.path.exists(os.path.join("image_processing","test_processed_images","bin","420.tiff"))
     
 def test_convert_tiff_image_converts_intermediate_files():
     #assert saved file matches 
-    target_bin = skimage.io.imread("image_processing//test_processed_images//targets//bin.png")
-    target_crop = skimage.io.imread("image_processing//test_processed_images//targets//crop.tiff")
-    target_bg_sub = skimage.io.imread("image_processing//test_processed_images//targets//bg_sub.tiff")
-    produced_bin = skimage.io.imread("image_processing//test_processed_images//bin//100.png")
-    produced_crop = skimage.io.imread("image_processing//test_processed_images//crop//100.tiff")
-    produced_bg_sub = skimage.io.imread("image_processing//test_processed_images//bg_sub//100.tiff")
+    target_bin = skimage.io.imread(os.path.join("image_processing","test_processed_images","targets","bin.tiff"))
+    target_crop = skimage.io.imread(os.path.join("image_processing","test_processed_images","targets","crop.tiff"))
+    target_bg_sub = skimage.io.imread(os.path.join("image_processing","test_processed_images","targets","bg_sub.tiff"))
+    produced_bin = skimage.io.imread(os.path.join("image_processing","test_processed_images","bin","420.tiff"))
+    produced_crop = skimage.io.imread(os.path.join("image_processing","test_processed_images","crop","420.tiff"))
+    produced_bg_sub = skimage.io.imread(os.path.join("image_processing","test_processed_images","bg_sub","420.tiff"))
     assert np.all(target_bin == produced_bin)
     assert np.all(target_crop == produced_crop)
     assert np.all(target_bg_sub == produced_bg_sub)
--- image_processing/tiff_handling.py
+++ image_processing/tiff_handling.py
@@ -1,10 +1,12 @@
+import numpy as np
+import os
+
 import skimage.filters
 import skimage.io
 import skimage.morphology
 from skimage.filters import (threshold_otsu, threshold_li)
 from skimage import exposure
-import numpy as np
-import os
+
 
 def define_initial_parameters():
     params_dict = dict(
@@ -66,6 +68,7 @@ def produce_background_image(background_video, params_dict):
     bg_median = exposure.rescale_intensity(bg_median, in_range='uint12')
     bg_median = skimage.img_as_int(bg_median)
     bg_median = bg_median[nozzle_row+crop_top:crop_bottom+crop_top, crop_width_start:crop_width_end]
+    
     return bg_median
                                                                   
 def convert_tiff_sequence_to_binary(experimental_sequence, bg_median, params_dict, save_location, save_crop=False,save_bg_subtract=False):
@@ -94,52 +97,41 @@ def subtract_background_single_image(cropped_image, bg_median):
     """Performs background subtraction from a cropped image. Assumes cropped_image and bg_median are the same size
     Returns an image."""
     background_subtracted_image = cropped_image - bg_median
-    background_subtracted_image = np.abs((background_subtracted_image < 0)*background_subtracted_image) 
+    background_subtracted_image = np.abs((background_subtracted_image > 0)*background_subtracted_image) 
+    background_subtracted_image = background_subtracted_image/np.max(background_subtracted_image)
     #eliminates half the noise
-    background_subtracted_image = np.uint16(background_subtracted_image)
+    background_subtracted_image = skimage.img_as_int(background_subtracted_image)
     return background_subtracted_image
 
-def li_binarize_single_image(background_subtracted_image):
+def otsu_binarize_single_image(background_subtracted_image):
     """Performs global binarization on an image according to the Li method 
     https://scikit-image.org/docs/dev/auto_examples/developers/plot_threshold_li.html
     """
-    thresh_li = threshold_li(background_subtracted_image)
-    binary_li = background_subtracted_image > thresh_li
-    binary_li = np.array(binary_li)*255
-    binary_li = np.uint8(binary_li)
-    return binary_li
+    thresh_otsu = threshold_otsu(background_subtracted_image)
+    binary_otsu = background_subtracted_image < thresh_otsu
+    binary_otsu = np.array(binary_otsu)*255
+    binary_otsu = np.uint8(binary_otsu)
+    return binary_otsu
 
-def save_image(image, image_number, save_location, save_crop=False, save_bg_subtract=False):
+def save_image(image, image_number, save_location):
     """Saves a single """
-    if save_crop:
-        filename = f"{image_number:03}.tiff"
-        full_filename = os.path.join(save_location,"crop",filename) 
-        skimage.io.imsave(full_filename, image, check_contrast=False)  
-        #save_crop = False
-        return False
-    if save_bg_subtract: 
-        filename = f"{image_number:03}.tiff"
-        full_filename = os.path.join(save_location,"bg_sub",filename) 
-        skimage.io.imsave(full_filename, image, check_contrast=False) 
-        #save_bg_subtract = False
-        return False
-    filename = f"{image_number:03}.png"
-    full_filename = os.path.join(save_location,"bin",filename) 
-    skimage.io.imsave(full_filename, image, check_contrast=False)
+    filename = f"{image_number:03}.tiff"
+    full_filename = os.path.join(save_location,filename) 
+    skimage.io.imsave(full_filename, image, check_contrast=False)  
     pass
 
 def convert_tiff_image(image, bg_median, params_dict, image_number, save_location, save_crop=False,save_bg_subtract=False):                          
     image = exposure.rescale_intensity(image, in_range='uint12')
     cropped_image = crop_single_image(image, params_dict)
     if save_crop: 
-        save_image(cropped_image, image_number, save_location, save_crop, save_bg_subtract)
+        save_image(cropped_image, image_number, os.path.join(save_location,"crop"))
         save_crop = False
     background_subtracted_image = subtract_background_single_image(cropped_image, bg_median)
     if save_bg_subtract:
-        save_image(background_subtracted_image, image_number, save_location, save_crop, save_bg_subtract)
+        save_image(cropped_image, image_number, os.path.join(save_location,"bg_sub"))
         save_bg_subtract=False
-    binary_image = li_binarize_single_image(background_subtracted_image)
-    save_image(binary_image, image_number, save_location, save_crop, save_bg_subtract)
+    binary_image = otsu_binarize_single_image(background_subtracted_image)
+    save_image(binary_image, image_number, os.path.join(save_location,"bin"))
     pass
 
         

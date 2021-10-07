435ffbc96804537576bba45c4bb7dc590a3435da
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2021-10-07 10:55:57 -0700

    begin csv-to_dataframe

--- data_processing/csv.py
+++ data_processing/csv.py
@@ -2,6 +2,8 @@ import pandas as pd
 import glob
 import typing
 import os
+from pathlib import Path
+import filehandling as fh
 
 def get_csvs(csv_location : typing.Union[str, bytes, os.PathLike]) -> list:
     """
@@ -15,19 +17,37 @@ def get_csvs(csv_location : typing.Union[str, bytes, os.PathLike]) -> list:
     Returns
     -------
     get_csvs : list
-        list of csvs in csv_location as path strings
+        sorted list of csvs in csv_location as path strings
 
     """
 
 
     csvs = glob.glob(os.path.join(csv_location,"*.csv"))
-    return csvs
+    return sorted(csvs)
 
 
+def csv_to_dataframe(csv : string,folder_name_format : str) -> pd.DataFrame:
+    """
+    """
 
-def generate_df(csv_location : typing.Union[str, bytes, os.PathLike]) -> pd.DataFrame:
+    df = pd.read_csv(csv)
+    f_name = Path(csv).name
+    params = fh.folder.folder_name_parse(f_name,)
 
-    df = pd.DataFrame()
+    pass
+
+def generate_df(csv_location : typing.Union[str, bytes, os.PathLike], fname_format : str, sample_info_format : str, tc_range : array) -> pd.DataFrame:
+    """
+
+    """
+
+    df_list = []
+    csvs = get_csvs(csv_location)
+    for csv in csvs:
+        sample_df = csv_to_dataframe(csv)
+        df_list.append(sample_df)
+    df = pd.concat(df_list)
+
     return df
 
     ## Current structure

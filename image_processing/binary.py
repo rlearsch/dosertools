d2a945528058b2ca69475309fa4c312887da02f4
Red Lhota <11944660+roseasromeo@users.noreply.github.com>
2021-10-13 13:50:24 -0700

    version of bottom_border with synthesized black and white code

--- image_processing/binary.py
+++ image_processing/binary.py
@@ -3,43 +3,89 @@ import skimage
 import data_processing as dp
 import file_handling as fh
 import typing
+import os
 
-def bottom_border(image : skimage.io.Image, white=True) -> int:
+def bottom_border(image : np.ndarray, white=True) -> int:
     """
+
+
+    Parameters
+    ----------
+
+
+    Returns
+
+
     """
 
     sum_rows = np.sum(image, axis = 1)
-
+    (height,width) = np.shape(image)
     if white:
-        # find_bottom_border_white
-        (height,width) = np.shape(image)
-        full_white_max = 255*width
-        max_rows = list(sum_rows == full_white_max)
-        if True in max_rows:
-            bottom_c = np.argmax(sum_rows) # returns first maximum
-
-        else:
-            white_runs = dp.array.continuous_nonzero(sum_rows)
-            if len(white_runs) == 1:
-                bottom_c = white_runs[0][1] - 10
-                # TODO: revise 10 to make less arbitrary
-            elif len(white_runs) > 1:
-                bottom_c = white_runs[-1][0]
-            else:
-                # len(white_runs) == 0
-                bottom_c = len(sum_rows)
+        # fully white rows are 0, all others are larger
+        zero_rows = 255*width - sum_rows
     else:
-        # find_bottom_border_black
-        if (sum_rows == 0).any(0):
-            bottom_c = dp.array.continuous_zero(sum_rows)[-1][0]
-        else:
-            bottom_c = len(sum_rows)
+        # fully black rows are already 0
+        zero_rows = sum_rows
+    if (zero_rows == 0).any(0):
+        # if there are any full rows of 0s, choose the start of the last block
+        # of 0 rows
+        # 0 is full white if white is True/default, full black if white=False
+        bottom_c = dp.array.continuous_zero(zero_rows)[-1][0]
+        # different from previous case for bottom_border_white
+        # bottom_c = np.argmax(sum_rows)
+        # where it picks the first all white row
+    else:
+        # by default, if there are no full rows, the bottom row will be the
+        # last row
+        bottom_c = len(sum_rows)
+        # check for other cropping scenarios
+        if white:
+            white_blocks = dp.array.continuous_nonzero(sum_rows)
+            if len(white_blocks) == 1:
+                # if there is only one long block of white (i.e. unbroken
+                # liquid bridge), but there are no full white rows, then
+                # try to correct for drop on substrate moving up from where
+                # it is located for background substration, and causing false
+                # minima
+                bottom_c = white_blocks[0][1] - 10
+                # TODO: revise 10 to make less arbitrary
+                # idea: look for local maximum, set local maximum as bottom row
+            elif len(white_blocks) > 1:
+                # if there are multiple blocks of white, pick the first row of
+                # the last one from the top
+                # from the top
+                bottom_c = white_blocks[-1][0]
     return bottom_c
+
+    # if white:
+    #     # find_bottom_border_white
+    #
+    #     full_white_max = 255*width
+    #     max_rows = list(sum_rows == full_white_max)
+    #     if True in max_rows:
+    #         bottom_c = np.argmax(sum_rows) # returns first maximum
+    #
+    #     else:
+    #         white_runs = dp.array.continuous_nonzero(sum_rows)
+    #         if len(white_runs) == 1:
+    #
+    #         elif len(white_runs) > 1:
+    #             bottom_c = white_runs[-1][0]
+    #         else:
+    #             # len(white_runs) == 0
+    #             bottom_c = len(sum_rows)
+    # else:
+    #     # find_bottom_border_black
+    #     if (sum_rows == 0).any(0):
+    #         bottom_c = dp.array.continuous_zero(sum_rows)[-1][0]
+    #     else:
+    #         bottom_c = len(sum_rows)
+    # return bottom_c
 
     # need to understand each portion
     # need test images that hit each condition
 
-def min_diameter(image : skimage.io.Image, window : np.array) -> int:
+def min_diameter(image : np.ndarray, window : np.array) -> int:
     """
     """
 
@@ -66,11 +112,9 @@ def min_diameter(image : skimage.io.Image, window : np.array) -> int:
         # include all values within 2 pixels of minimum in average
         # avoids effects due to arbitrary stepping from the discrete nature of
         # pixels
-        min_diameter_avg = np.mean([
-                                value
-                                for i, value in enumerate(diameter_profile)
-                                if value <= (min(diameter_profile)+2)])
-                                ])
+        min_diameter_avg = np.mean([value \
+            for i, value in enumerate(diameter_profile) \
+            if value <= (min(diameter_profile)+2)])
     return min_diameter_avg
 
 def time_diameter_data(binary_location : typing.Union[str, bytes, os.PathLike], window : np.array, fps : int, nozzle_row=1):

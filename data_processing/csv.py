cdd5601334276fe3e4869a97dc937a6a7029ca4c
Red Lhota <11944660+roseasromeo@users.noreply.github.com>
2021-10-20 14:43:20 -0700

    Add minor alterations to comments to bring into style

--- data_processing/csv.py
+++ data_processing/csv.py
@@ -23,14 +23,14 @@ def get_csvs(csv_location : typing.Union[str, bytes, os.PathLike]) -> list:
         sorted list of csvs in csv_location as path strings
     """
 
-    # Because glob does not return the files in any particular order, sort
+    # Because glob does not return the files in any particular order, sorts
     # before returning to keep consistent order between runs.
     csvs = glob.glob(os.path.join(csv_location,"*.csv"))
     return sorted(csvs)
 
 def csv_to_dataframe(csv : str, tc_bounds : np.array, fname_format : str, sampleinfo_format : str,fname_split="_", sample_split='-') -> pd.DataFrame:
     """
-    Read in a csv into a dataframe with sample parameters.
+    Reads in a csv into a dataframe with sample parameters.
 
     Parameters
     ----------
@@ -61,15 +61,15 @@ def csv_to_dataframe(csv : str, tc_bounds : np.array, fname_format : str, sample
 
     dataset = pd.read_csv(csv)
 
-    # Truncate the data before the longest block of zeros.
+    # Truncates the data before the longest block of zeros.
     dataset = extension.truncate_data(dataset)
-    # Add the strain rate to the dataset.
+    # Adds the strain rate to the dataset.
     dataset = extension.add_strain_rate(dataset)
 
-    # Find critical time by locating the maximum strain rate within the bounds.
+    # Finds critical time by locating the maximum strain rate within the bounds.
     dataset = extension.add_critical_time(dataset, tc_bounds)
 
-    # Read in parameters from file name and add to dataframe.
+    # Reads in parameters from file name and add to dataframe.
     fname = Path(csv).name
     params = fh.folder.parse_filename(fname,fname_format,sampleinfo_format,fname_split,sample_split)
     for key, value in params.items():
@@ -79,9 +79,9 @@ def csv_to_dataframe(csv : str, tc_bounds : np.array, fname_format : str, sample
 
 def generate_df(csv_location : typing.Union[str, bytes, os.PathLike], tc_bounds : np.array, fname_format : str, sampleinfo_format : str, fname_split="_", sample_split='-') -> pd.DataFrame:
     """
-    Read in all csvs and process them into a dataframe.
+    Reads in all csvs and process them into a dataframe.
 
-    Read in data from all csvs in csv_location, process each, adding
+    Reads in data from all csvs in csv_location, process each, adding
     strain rate, critical time, radius at critical time, and parameters from the
     filename, and put all data into one dataframe. Loops csv_to_dataframe for
     all csvs in folder.
@@ -115,7 +115,7 @@ def generate_df(csv_location : typing.Union[str, bytes, os.PathLike], tc_bounds 
     df_list = []
     csvs = get_csvs(csv_location)
 
-    # Run the processing for each csv in the folder.
+    # Runs the processing for each csv in the folder.
     for csv in csvs:
         sample_df = csv_to_dataframe(csv,tc_bounds,fname_format,sampleinfo_format,fname_split,sample_split)
         df_list.append(sample_df)

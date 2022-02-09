1a924469dfea8e1aba97b1ad5921907bd7b9ac76
Robert Learsch <rlearsch@caltech.edu>
2022-02-09 13:37:56 -0800

    Added docstrings to data_processing\figures

--- src/dostools/data_processing/figures.py
+++ src/dostools/data_processing/figures.py
@@ -17,12 +17,17 @@ def layout_time_csvs(df: pd.DataFrame, plot_normalized: bool) -> hv.Points:
 
     Parameters
     ----------
-    df
-    plot_normalized
+    df: pd.DataFrame
+        Dataframe of D/D0 and time data with sample and run information
+
+    plot_normalized: bool
+        True to normalize time by t_c, the critical time, and plot t - tc on the x-axis
+        False to plot raw time on the x-axis
 
     Returns
     -------
-
+    hv_layout: hv.Points
+        Set of plots for each run and sample included in df
     """
     key_dimensions = ["time (s)", "D/D0"]
     if plot_normalized:
@@ -44,7 +49,20 @@ def layout_time_csvs(df: pd.DataFrame, plot_normalized: bool) -> hv.Points:
     return hv_layout
 
 def layout_viscosity_csvs(df: pd.DataFrame) -> hv.Points:
+    """
+    Plots a strain vs (elongational viscosity / surface tension) graph of all samples and runs in df.
 
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe with (elongational viscosity / surface tension) and strain with sample and run information
+
+    Returns
+    -------
+    hv_layout: hv.Points
+        Set of plots for each run and sample included in df
+    """
     hv_layout = hv.Points(
         data = df,
         kdims=["strain", "(e visc / surface tension) (s/m)"],
@@ -61,6 +79,33 @@ def layout_viscosity_csvs(df: pd.DataFrame) -> hv.Points:
 
 def save_figure(figure: hv.Points, figure_name: str, summary_folder: typing.Union[str, bytes, os.PathLike],
                 optional_settings: dict = {}) -> None:
+    """
+    Saves the figure as an .html file which enables interactivity
+
+    Parameters
+    ----------
+    figure: hv.Points
+        Set of plots for each run and sample included in df
+    figure_name: string
+        Filename with which to save the figure
+    summary_folder: Path
+        Location to save the figure
+    optional_settings: dict
+        Dictionary of optional settings.
+
+    Optional Settings and Defaults
+    ------------------------------
+    verbose: bool
+        Determines whether processing functions print statements as they
+        progress through major steps. True to see print statements, False to
+        hide non-errors/warnings.
+        Default is False.
+
+    Returns
+    -------
+    None, file saved to disk
+    """
+
     settings = integration.set_defaults(optional_settings)
     verbose = settings["verbose"]
 
